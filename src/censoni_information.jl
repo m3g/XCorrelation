099b620af6fe8e7eccec528e1bf8c33fda9836f4
Luciano Censoni (laptop) <luciano.censoni@gmail.com>
2019-10-15 22:42:20 +0200

    Added alternative f_censoni based on analytical result rather than numerical integration. abs( f_censoni(rmin, rmax, l) - f_censoni(rmin, rmax, l, step=10^-N) ) < 3.10^-N for all values tested.

--- src/censoni_information.jl
+++ src/censoni_information.jl
@@ -1,20 +1,41 @@
-euclidean_distance(x,y) = sqrt( (x[1]-y[1])^2 + (x[2]-y[2])^2 + (x[3]-y[3])^2 )
+using SpecialFunctions
+
+
+euclidean_distance(x, y) = sqrt( (x[1]-y[1])^2 + (x[2]-y[2])^2 + (x[3]-y[3])^2 )
+gamma_inc_prec(x, y) = gamma(x) * (1.0-gamma_inc(x, y, 0)[1]) # high precision, first output, matches Wolfram definition
+
+
+function F_censoni(r, l)
+  # primitive
+  Al2v = 16.58 * (l^1.15)
+  B = 2.4343
+  C = 0.8555
+  D = -0.3125
+  E = 0.9375
+  return 1.0 + (B * D * gamma_inc_prec( E, (C/Al2v)^1.6 * r^3.2)) / C^1.5
+end
+
 
 function f_censoni(rmin, rmax, l)
-  step = 0.0001
-  Al2v = 16.58*(l^1.15)
+  return -log2( F_censoni( rmax, l) - F_censoni(rmin, l))
+end
+
+
+function f_censoni(rmin, rmax, l, stp)
+  # numerical integration, reasonable step is: stp = 0.0001
+  Al2v = 16.58 * (l^1.15)
   f = 0.
   s = rmin
   while s < rmax
     r2 = s^2
-    f = f +( 2.4343 / Al2v^(1.5) )*exp( -(0.8555 * r2/Al2v)^1.6)*r2*step
-    s = s + step
+    f = f +( 2.4343 / Al2v^(1.5) )*exp( -(0.8555 * r2/Al2v)^1.6) * r2 * stp
+    s = s + stp
   end
   return -log2(f)
 end
 
-function censoni_information(pdbfile)
 
+function censoni_information(pdbfile)
   pdb = JPleXL.readPDB(pdbfile)
   cas = JPleXL.xCA(pdb)
 
@@ -35,6 +56,4 @@ function censoni_information(pdbfile)
   end
 
   return information
-
 end
-
